e0e9aabf7ce335cb71224f96836c547c457e4623
JulioBeltran <julio96.jb@gmail.com>
2024-03-20 19:02:12 -0700

    Resolve exercise 1 'directivas estructurales'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularBootCamp';
+
+  active: boolean = false;
+
+  toggleActive() {
+    this.active = !this.active;
+  }
 }
