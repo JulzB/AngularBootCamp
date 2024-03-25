b3cffb0b95ceeaadd232d8e4bb8143eb66bd01bd
JulioBeltran <julio96.jb@gmail.com>
2024-03-25 08:15:08 -0700

    Resolve exercise 2

---
+++ src/app/homework2/homework2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homework2',
+  templateUrl: './homework2.component.html',
+  styleUrl: './homework2.component.scss'
+})
+export class Homework2Component {
+
+  strings: string[] = ['A', 'B', 'C','C'];
+
+}
